910257679cfb98aec8808c9e47af0490e2350bda
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-12-16 16:25:57 -0800

    add rudderstack plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: "Rudderstack-Plugin",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-rudderstack`,
+      options: {
+        // your rudderstack write key for your production environment
+        // when process.env.NODE_ENV === 'production'
+        // required; non-empty string
+        //NOTE: Do not commit this to git. Process from env.
+        prodKey: `1l7YzB8IxCUDhsq7Imxcxj3MnvH`,
+        devKey: `1l7YzB8IxCUDhsq7Imxcxj3MnvH`,
+        trackPage: true,
+      },
+    },
+  ],
 };
